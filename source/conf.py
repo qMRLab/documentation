c6cdf4af41d8c68a6b55ff869085bd5c2904599c
Agah Karakuzu <agahkarakuzu@gmail.com>
2022-02-11 14:51:40 +0300

    more docs

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosectionlabel']
+    'sphinx.ext.autosectionlabel',
+    'sphinx_togglebutton']
     #'sphinxcontrib.matlab']
 
 # Add any paths that contain templates here, relative to this directory.
